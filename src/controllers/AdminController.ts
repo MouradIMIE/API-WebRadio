52976071de2d466a4d9aadbff0184979a0e778a8
MouradBoukhalfi <mouradboukhalfi3@gmail.com>
2021-04-04 15:08:35 +0200

    RAD-6 Route login work added also jwtutils

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import emailHelper from "../helpers/emailHelper";
-import { hashPassword } from "../helpers/passwordHelper";
+import { comparePassword, hashPassword } from "../helpers/passwordHelper";
 import { adminInterface } from "../interfaces/adminInterface";
 import { Admin } from "../models/adminModel";
 import { adminUtils } from "../utils/adminUtils";
+import { jwtUtils } from "../utils/jwtUtils";
 import sendMail from "../utils/mailer";
 
 export class AdminController {
+// -------------------------------------------------REGISTER---------------------------------------------------//
   static register = async (req: Request, res: Response) => {
     try {
       const body: adminInterface = req.body;
@@ -35,12 +37,14 @@ export class AdminController {
       const passwordEncrypted: string = await hashPassword(password);
 
       //Création de l'admin
-      const admin: adminInterface = await Admin.create({
+      const admin = await Admin.create({
         firstname: body.firstname,
         lastname: body.lastname,
         email: body.email,
         password: passwordEncrypted,
-        createdBy: body.createdBy
+        createdBy: body.createdBy,
+        token:"",
+        refreshToken:""
       });
 
       //Envoie de la response
@@ -83,6 +87,68 @@ export class AdminController {
       }
     }
   };
+// --------------------------------------------------LOGIN----------------------------------------------------//
+  static login = async (req: Request, res: Response) => {
+    try {
 
-  static login = async (req: Request, res: Response) => {};
+      const body: adminInterface = req.body;
+
+      //Verification qu'on envoie bien un email et un password
+      if(!body.email || !body.password) throw new Error("Des données sont manquantes");
+      
+      //Vérification de si l'email est au bon format
+      if (!emailHelper.validEmail(body.email))
+        throw new Error("Le format de l'email n'est pas valide");
+
+      //Vérification que l'utilisateur existe en base 
+      let admin = await Admin.findOne({email : body.email});
+      if(!admin) throw new Error("Cet email n'est associé à aucun compte");
+
+      //Vérification du mot de passe
+      if(!await comparePassword(body.password,admin.password)) throw new Error("Le mot de passe n'est pas correct")
+
+      //Génération des tokens
+
+      admin = await jwtUtils.generateAdminToken(admin);
+      admin = await jwtUtils.generateAdminRefreshToken(admin);
+
+      res.status(200).send({
+        error: false,
+        message: "L'admin a été ajouté avec succès",
+        admin: {
+          id: admin._id,
+          firstname: admin.firstname,
+          lastname: admin.lastname,
+          email: admin.email,
+          token: admin.token,
+          refreshToken: admin.refreshToken
+        },
+      });
+    } catch (error) {
+      if (error.message === "Le format de l'email n'est pas valide") {
+        res.status(400).send({
+          error: true,
+          message: "Le format de l'email n'est pas valide"
+        });
+      }
+      if (error.message === "Des données sont manquantes") {
+        res.status(400).send({
+          error: true,
+          message: "Des données sont manquantes"
+        });
+      }
+      if (error.message === "Cet email n'est associé à aucun compte") {
+        res.status(400).send({
+          error: true,
+          message: "Cet email n'est associé à aucun compte"
+        });
+      }
+      if (error.message === "Le mot de passe n'est pas correct") {
+        res.status(400).send({
+          error: true,
+          message: "Le mot de passe n'est pas correct"
+        });
+      }
+    }
+  };
 }
