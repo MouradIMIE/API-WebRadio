5ffd300c950bad88ce8e04368973231b1896da69
MouradBoukhalfi <mouradboukhalfi3@gmail.com>
2021-04-04 12:25:31 +0200

    Finish last review requests

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -9,34 +9,38 @@ import sendMail from "../utils/mailer";
 export class AdminController {
   static register = async (req: Request, res: Response) => {
     try {
-      const { firstname, lastname, email, createdBy } = req.body;
+      const body: adminInterface = req.body;
 
       //Génération aléatoire d'un mot de passe qui sera stocké dans la base de donnée et envoyer et
       //par mail en cas de succès d'inscription à l'utilisateur
       const password: string = adminUtils.generateAdminPassword();
 
       // Vérification de si toutes les données existent
-      if (!firstname || !lastname || !email || !createdBy)
+      if (!body.firstname || !body.lastname || !body.email || !body.createdBy)
         throw new Error("Des données sont manquantes");
 
       //Vérification de si l'email est au bon format
-      if (!emailHelper.validEmail(email))
+      if (!emailHelper.validEmail(body.email))
         throw new Error("Le format de l'email n'est pas valide");
 
       //Vérification de si l'email n'existe pas déjà
-      if (await adminUtils.existingEmail(email))
+      if (await adminUtils.existingEmail(body.email))
         throw new Error("Cet email existe déjà");
+
+      //Vérification de si l'admin existe en base de données
+      if (!(await Admin.findOne({ createdBy: body.createdBy })))
+        throw new Error("Vous n'avez pas accès a cette fonctionnalité");
 
       //Hash le password avant de le send en base de donnée
       const passwordEncrypted: string = await hashPassword(password);
 
       //Création de l'admin
       const admin: adminInterface = await Admin.create({
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
+        firstname: body.firstname,
+        lastname: body.lastname,
+        email: body.email,
         password: passwordEncrypted,
-        createdBy: createdBy
+        createdBy: body.createdBy
       });
 
       //Envoie de la response
@@ -69,6 +73,12 @@ export class AdminController {
         res.status(400).send({
           error: true,
           message: "Cet email existe déjà"
+        });
+      }
+      if (error.message === "Vous n'avez pas accès a cette fonctionnalité") {
+        res.status(400).send({
+          error: true,
+          message: "Vous n'avez pas accès a cette fonctionnalité"
         });
       }
     }
--- src/interfaces/adminInterface.ts
+++ src/interfaces/adminInterface.ts
@@ -1,9 +1,9 @@
 export interface adminInterface{
-    _id:string,
+    _id?:string,
     firstname:string,
     lastname:string,
     email:string,
-    password:string,
+    password?:string,
     createdBy:string,
     token?: string;
     refreshToken?: string;
